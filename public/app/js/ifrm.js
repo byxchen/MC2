c8f420947d2e2bac8acea6d29e838a9a948d1576
Brian <catzwillruletheearth@gmail.com>
2017-06-30 00:56:12 -0400

    commit for combined stuff

---
+++ public/app/js/ifrm.js
@@ -0,0 +1,13 @@
+function swapFrame(){
+	var chat = document.getElementById("chatframe");
+	var editor = document.getElementById("editorframe");
+	editor.style.display = "inline";
+	chat.style.display = "none";
+}
+
+function swapParentFrame(){
+	var chat = parent.document.getElementById("chatframe");
+	var editor = parent.document.getElementById("editorframe");
+	editor.style.display = "none";
+	chat.style.display = "inline";
+}
