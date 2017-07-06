33f0304384d4abd5a9a96d2efcd2e874e77b13a5
Brian <catzwillruletheearth@gmail.com>
2017-07-06 10:24:55 -0400

    upload test key changes

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,9 +1,9 @@
 var App = angular.module('ChatRoom',['ngResource','ngRoute','ngStorage','socket.io','ngFileUpload','Controllers','Services'])
 .run(["$rootScope", function ($rootScope){
-	$rootScope.baseUrl = 'http://142.1.89.43:8080'; //Application URL
+	$rootScope.baseUrl = 'http://192.168.10.8:8080'; //Application URL
 }]);
 App.config(function ($routeProvider, $socketProvider){
-	$socketProvider.setConnectionUrl('http://142.1.89.43:8080'); // Socket URL
+	$socketProvider.setConnectionUrl('http://192.168.10.8:8080'); // Socket URL
 
 	$routeProvider	// AngularJS Routes
 	.when('/v1/', {
