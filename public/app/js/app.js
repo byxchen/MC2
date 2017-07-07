15f1a7934aac3b4523a8b1c7cad063366776fb12
bestlkh <bestlkh@users.noreply.github.com>
2017-07-06 23:30:10 -0400

    Update app.js

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,5 +1,5 @@
-const PORT = process.env.PORT || 3000;
-const BUrl =  process.env.BaseUrl || 'http://localhost';
+const PORT = process.env.PORT || 8080;
+const BUrl =  process.env.BaseUrl || 'https://ice-mc2.herokuapp.com/';
 
 const FUrl = BUrl + ":" + PORT;
 
