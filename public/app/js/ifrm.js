944ce0d486e6d1db22bb720e8b8185c6b6002386
Brian <catzwillruletheearth@gmail.com>
2017-07-07 09:54:15 -0400

    hides swap button for non-mobile

--- public/app/js/ifrm.js
+++ public/app/js/ifrm.js
@@ -1,6 +1,8 @@
 function swapFrame(){
 	var chat = document.getElementById("chatframe");
 	var editor = document.getElementById("editorframe");
+	var ts = document.getElementById('editor-frame').contentWindow.document.getElementById('tool_swap');
+	ts.style.display = "block";
 	editor.style.display = "inline";
 	chat.style.display = "none";
 }
