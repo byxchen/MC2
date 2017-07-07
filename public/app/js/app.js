4ce5c9d9412e99447af78063caa6d5ff6b6d1808
bestlkh <bestlkh@users.noreply.github.com>
2017-07-06 21:24:42 -0400

    Update app.js

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,9 +1,9 @@
 var App = angular.module('ChatRoom',['ngResource','ngRoute','ngStorage','socket.io','ngFileUpload','Controllers','Services'])
 .run(["$rootScope", function ($rootScope){
-	$rootScope.baseUrl = 'https://ice-mc2.herokuapp.com/'; //Application URL
+	$rootScope.baseUrl = 'https://localhost:8080/'; //Application URL
 }]);
 App.config(function ($routeProvider, $socketProvider){
-	$socketProvider.setConnectionUrl('https://ice-mc2.herokuapp.com/'); // Socket URL
+	$socketProvider.setConnectionUrl('https://localhost:8080/'); // Socket URL
 
 	$routeProvider	// AngularJS Routes
 	.when('/v1/', {
