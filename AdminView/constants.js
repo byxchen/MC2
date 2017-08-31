f57c2f127ba830418d1a0aac37e63ac918099ecb
ZiXian(Wannie) Chen <Wannie923@gmail.com>
2017-08-31 11:31:50 -0400

    point to remote db

--- AdminView/constants.js
+++ AdminView/constants.js
@@ -1,7 +1,7 @@
 var constants = {
     adminIndexPath: "public/admin/index.html",
     views: ["dashboard", "chat"],
-    dbUrl: "mongodb://localhost:27017/control"
+    dbUrl: "mongodb://gradutree.me:3001/control"
 };
 
 module.exports = constants;
